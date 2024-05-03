0a5995eb58aeec5d2c8dd3e6c098285ac405c2d4
csehg <csehgabor451@gmail.com>
2024-05-03 21:07:07 +0200

    pictures are loaded

--- src/components/BarcodeReader.vue
+++ src/components/BarcodeReader.vue
@@ -1,64 +1,61 @@
 <script setup>
-import DataVisualizer from './DataVisualizer.vue';
+import DataVisualizer from "./DataVisualizer.vue";
 </script>
 <template>
-    <div>
-      <h2>Image input:</h2>
-      <input type="file" accept="image/*" @change="handleFileInput">
-      <h2>User input:</h2>
-      <input type="number" v-model="barcodeData" />
-      <div v-if="barcodeData">
-        <p>Barcode Data: {{ barcodeData }}</p>
-      </div>
-      <DataVisualizer :barcode="barcodeData" />
+  <div>
+    <h2>Image input:</h2>
+    <input type="file" accept="image/*" @change="handleFileInput" />
+    <h2>User input:</h2>
+    <input type="text" v-model="barcodeData" />
+    <div v-if="barcodeData">
+      <p>Barcode Data: {{ barcodeData }}</p>
     </div>
-  </template>
-  
-  <script>
-  import Quagga from 'quagga';
-  
-  export default {
-    data() {
-      return {
-        barcodeData: null
-      };
-    },
-    methods: {
-      handleUserInput(){
+    <DataVisualizer :barcode="barcodeData" />
+  </div>
+</template>
 
-      },
-      handleFileInput(event) {
-        const file = event.target.files[0];
-        if (file) {
-          this.decodeBarcode(file);
-        }
-      },
-      decodeBarcode(imageFile) {
-        Quagga.decodeSingle(
-          {
-            src: URL.createObjectURL(imageFile),
-            locate: true,
-            inputStream: {
-              size: 800
-            },
-            decoder: {
-              readers: ["ean_reader"] // specify the barcode type you want to scan
-            }
-          },
-          (result) => {
-            if (result && result.codeResult) {
-              this.barcodeData = result.codeResult.code;
-            } else {
-              this.barcodeData = "No barcode detected";
-            }
-          }
-        );
+<script>
+import Quagga from "quagga";
+
+export default {
+  data() {
+    return {
+      barcodeData: null,
+    };
+  },
+  methods: {
+    handleUserInput() {},
+    handleFileInput(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.decodeBarcode(file);
       }
-    }
-  };
-  </script>
-  
-  <style>
-  /* Add any additional styling here */
-  </style>
-  
+    },
+    decodeBarcode(imageFile) {
+      Quagga.decodeSingle(
+        {
+          src: URL.createObjectURL(imageFile),
+          locate: true,
+          inputStream: {
+            size: 800,
+          },
+          decoder: {
+            readers: ["ean_reader"], // specify the barcode type you want to scan
+          },
+        },
+        (result) => {
+          if (result && result.codeResult) {
+            this.barcodeData = result.codeResult.code;
+          } else {
+            this.barcodeData = "No barcode detected";
+          }
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style>
+/* Add any additional styling here */
+</style>
--- src/components/DataVisualizer.vue
+++ src/components/DataVisualizer.vue
@@ -7,21 +7,24 @@ const props = defineProps({
 });
 
 const apiUrl = "https://world.openfoodfacts.net/api/v2/product";
-const apiFields = "?fields=product_name,nutrition_grades,nutriscore_data";
+const apiFields =
+  "?fields=product_name,nutrition_grades,nutriscore_data,nutrition,images";
 
 const product = ref(null);
 const errorMessage = ref(null);
+const imageUrl = ref(null);
 
 const loadProduct = async () => {
   product.value = null;
   errorMessage.value = null;
+  imageUrl.value = null;
   try {
     if (!props.barcode) {
       throw new Error("barcode is null");
     }
 
     const response = await axios.get(`${apiUrl}/${props.barcode}${apiFields}`);
-    console.log(response.data);
+    // console.log(response.data);
 
     if (!response.data.product) {
       throw new Error(`Product data is null`);
@@ -43,6 +46,34 @@ const loadProduct = async () => {
       nutrition_grades: response.data.product.nutrition_grades,
       nutrition_data: extractedData,
     };
+
+    const getImageUrl = (productData, imageName, resolution = "full") => {
+      if (
+        !productData.product.images ||
+        !productData.product.images[imageName]
+      ) {
+        return null;
+      }
+      const baseUrl = "https://images.openfoodfacts.org/images/products";
+      let prodcode = productData.code;
+      if (prodcode.length > 8) {
+        prodcode = prodcode.replace(/(...)(...)(...)(.*)/, "$1/$2/$3/$4");
+      }
+      let filename;
+      if (/^\d+$/.test(imageName)) {
+        const resolutionSuffix = resolution === "full" ? "" : `.${resolution}`;
+        filename = `${imageName}${resolutionSuffix}.jpg`;
+      } else {
+        const rev = productData.product.images[imageName].rev;
+        filename = `${imageName}.${rev}.${resolution}.jpg`;
+      }
+      return `${baseUrl}/${prodcode}/${filename}`;
+    };
+
+    const productData = response.data;
+    const imageName = 1;
+
+    imageUrl.value = getImageUrl(productData, imageName, 400);
   } catch (error) {
     errorMessage.value = `Error fetching product data: ${error.message}`;
   }
@@ -54,6 +85,7 @@ const loadProduct = async () => {
     <button @click="loadProduct">Load product</button>
   </div>
   <div id="product-info">
+    <img v-if="imageUrl" :src="imageUrl" />
     <p v-if="product">Product name: {{ product.product_name }}</p>
     <p v-if="product">
       Nutrition grade: {{ product.nutrition_grades.toUpperCase() }}
@@ -76,7 +108,7 @@ const loadProduct = async () => {
       <span class="value"> {{ product.nutrition_data.saturated_fat }} g </span>
     </div>
     <div v-if="product" class="item">
-      <span class="label">Energy: </span>
+      <span class="label">Calories: </span>
       <span class="value"> {{ product.nutrition_data.energy }} kJ </span>
     </div>
     <p v-if="errorMessage">Error: {{ errorMessage }}</p>
